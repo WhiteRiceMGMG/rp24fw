fede167f779224b62cd4e58367b678fcb5036df3
ogawa <ocmikanmgmg@gmail.com>
2026-01-21 08:29:38 +0900

    drv

--- rp24fw_v1/drv/drv/rp24_drv.c
+++ rp24fw_v1/drv/drv/rp24_drv.c
@@ -1,28 +1,30 @@
 #include "./rp24_drv.h"
 
-rp24Driver Rp24 =
+rp24Driver rp24 =
 {
-    .Btn =
+    .firstbtn =
     {
-        .On = Button_On
+        .On = u1g_firstbtn_conf
     },
-    .Led =
+
+    .secondbtn =
+    {
+        .On = u1g_secondbtn_conf
+    },
+
+    .thirdbtn =
+    {
+        .On = u1g_thirdbtn_conf
+    },
+
+    .fourthbtn =
+    {
+        .On = u1g_fourthbtn_conf
+    },
+
+    .firstled =
     {
         .On = Led_On,
         .Off = Led_Off
     }
-};#ifndef RP24_DRV_H
-#define RP24_DEV_H
-
-#include <rp24knl.h>
-#include "../btn/btn_drv.h"
-#include "../led/led_drv.h"
-
-typedef struct
-{
-    btnDriver Btn;
-    ledDriver Led;
-}rp24Driver;
-
-extern rp24Driver Rp24;
-#endif
+};
